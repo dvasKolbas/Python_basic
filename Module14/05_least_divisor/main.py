761fbb2d6f57360be616736b581eaa67d5919bbe
dvasKolbas <karelin174@gmail.com>
2021-10-22 00:33:54 +0500

    Решение пятого задания

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def find_divider(N):
+    res = 0
+    for i in range(2, N+1):
+        if N % i == 0:
+            res = i
+            return i
+
+N = int(input("Введите число: "))
+d = find_divider(N)
+print("Наименьший делитель, отличный от единицы:", d)
