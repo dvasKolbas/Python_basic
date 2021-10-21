14926209ff59b12ba2bb80575aea67ea0a1f7861
dvasKolbas <karelin174@gmail.com>
2021-10-22 00:02:08 +0500

    Решение третьего задания

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+N = int(input("Введите число: "))
+summ = 0
+count = 0
+for i in str(N):
+    summ += int(i)
+    count += 1
+print("Сумма цифр:", summ)
+print("Кол-во цифр в числе:", count)
+print("Разность суммы и кол-ва цифр:", summ - count)
