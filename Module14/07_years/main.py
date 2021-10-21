341c2d2283a867812ef60886fa6800319c4bf2fa
dvasKolbas <karelin174@gmail.com>
2021-10-22 01:15:22 +0500

    Решение седьмого задания

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+def print_year(start_year, stop_year):
+    print(f"Года от {start_year} до {stop_year} с тремя одинаковыми цифрами:")
+    for i in range(start_year, stop_year):
+        if check_year(i):
+            print(i)
+
+def check_year(year):
+    a = year % 10
+    b = (year // 10) % 10
+    c = (year // 100) % 10
+    d = year // 1000
+    count = 0
+    for i in range(2):
+        if a == b:
+           count += 1
+        if a == c:
+            count += 1
+        if a == d:
+            count += 1
+        if count == 2:
+            return True
+        else:
+            count = 0
+            a, b = b, a
+    return False
+
+start_year = int(input("Введите первый год: "))
+stop_year = int(input("Введите второй год: "))
+print_year(start_year, stop_year)
