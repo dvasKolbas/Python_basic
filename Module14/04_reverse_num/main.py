c11b497de65ef32f14aa213e7d4516c9bc1c12dd
dvasKolbas <karelin174@gmail.com>
2021-10-22 00:26:03 +0500

    Решение четвертого задания

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,25 @@
-# TODO здесь писать код
+def change_num(value):
+    change = True
+    a = b = ""
+    for i in value:
+        if i == ".":
+            change = False
+        else:
+            if change:
+                a = i + a
+            else:
+                b = i + b
+    return float(a + "." + b)
+
+N = float(input("Введите первое число: "))
+new_N = change_num(str(N))
+K = float(input("Введите второе число: "))
+new_K = change_num(str(K))
+
+print("\nПервое число наоборот:", new_N)
+print("Второе число наоборот:", new_K)
+print("Сумма:", new_N + new_K)
+
+
 
 
