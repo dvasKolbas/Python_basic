80a14e1085477340433029644d5bdca2f01a45f6
dvasKolbas <karelin174@gmail.com>
2021-10-22 00:45:28 +0500

    Решение шестого задания

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import math
+
+def check_coin(a, b, r):
+    c = math.sqrt(a**2 + b**2)
+    if c <= r:
+        return True
+    else:
+        return False
+
+print("Введите координаты монетки:")
+x = float(input("X: "))
+y = float(input("Y: "))
+r = int(input("Введите радиус: "))
+coin = check_coin(abs(x), abs(y), r)
+
+if coin:
+    print("\nМонетка где-то рядом")
+else:
+    print("\nМонетки в области нет")
