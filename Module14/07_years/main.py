4a79386664096f0cb47d9b0252bc488b12a4d72d
dvasKolbas <karelin174@gmail.com>
2021-10-23 00:24:36 +0500

    Переделал проверку года

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -5,24 +5,30 @@ def print_year(start_year, stop_year):
             print(i)
 
 def check_year(year):
-    a = year % 10
-    b = (year // 10) % 10
-    c = (year // 100) % 10
-    d = year // 1000
-    count = 0
-    for i in range(2):
-        if a == b:
-           count += 1
-        if a == c:
-            count += 1
-        if a == d:
-            count += 1
-        if count == 2:
-            return True
-        else:
-            count = 0
-            a, b = b, a
-    return False
+    year_list = list(str(year))
+    if year_list.count(year_list[0]) == 3 or year_list.count(year_list[1]) == 3:
+        return True
+    else:
+        return False
+    # a = year % 10
+    # b = (year // 10) % 10
+    # c = (year // 100) % 10
+    # d = year // 1000
+    # count = 0
+    # for i in range(2):
+    #     if a == b:
+    #        count += 1
+    #     if a == c:
+    #         count += 1
+    #     if a == d:
+    #         count += 1
+    #     if count == 2:
+    #         return True
+    #     else:
+    #         count = 0
+    #         a, b = b, a
+    # return False
+
 
 start_year = int(input("Введите первый год: "))
 stop_year = int(input("Введите второй год: "))
