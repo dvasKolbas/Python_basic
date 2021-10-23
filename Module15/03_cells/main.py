a8da6bc6dc38971f5e0cc2931e17502a65f9ad55
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:06:01 +0500

    Выполнил 3 задание 15 модуля

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def input_cells(count, cells):
+    for i in range(count):
+        cells.append(int(input("Эффективность " + str(i + 1) + " клетки: ")))
+    return cells
+
+
+def print_cells(cells):
+    print("\nНеподходящие значения:", end=" ")
+    for i in range(len(cells)):
+        if cells[i] < i:
+            print(cells[i], end=" ")
+
+
+count = int(input("Введите кол-во клеток: "))
+cells = []
+cells = input_cells(count, cells)
+print_cells(cells)
