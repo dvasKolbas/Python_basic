94fa7523885ee2ea573fbe6bb599f05ecc54cce8
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:04:56 +0500

    Выполнил 1 задание 15 модуля

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def create_list(N):
+    odd_num = []
+    for i in range(1, N + 1, 2):
+        odd_num.append(i)
+    print(odd_num)
+
+
+N = int(input("Введите число: "))
+create_list(N)
