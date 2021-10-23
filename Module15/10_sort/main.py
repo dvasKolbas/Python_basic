1b4d3bc3bf7d5bff0afd077cd4340b945b2218e7
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:09:09 +0500

    Выполнил 10 задание 15 модуля

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+N = [1, 5, 2, 10, 7, 9]
+count = 0
+for j in range(len(N) - 1):
+  count +=1
+  stop = True
+  for i in range(len(N) - 1):
+    if N[i] > N[i +1]:
+      N[i], N[i + 1] = N[i + 1], N[i]
+      stop = False
+  if stop:
+    break
+print(count)
+print(N)
