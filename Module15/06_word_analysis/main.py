3fa0ae595f1e8167cc5209eaee985faf408e1c72
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:07:25 +0500

    Выполнил 6 задание 15 модуля

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def count_letter(word):
+  count = 0
+  word_list = list(word)
+  for i in range(len(word_list)):
+    unique = True
+    for j in range(len(word_list)):
+      if i != j and word_list[i] == word_list[j]:
+        unique = False
+        break
+    if unique:
+      count += 1
+  print("Кол-во уникальных букв:", count)
+
+word = input("Введите слово: ")
+count_letter(word)
