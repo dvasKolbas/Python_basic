70c07597b986eba32d6dc8a7858795ab1479d220
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:08:44 +0500

    Выполнил 9 задание 15 модуля

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+list_word = list(word)
+first_part = ""
+second_part = ""
+for i in range(len(list_word) // 2):
+  first_part += list_word[i]
+  second_part = list_word[i] + second_part
+if len(list_word) % 2 != 0:
+  first_part += list_word[len(list_word) // 2] + second_part
+else:
+  first_part += second_part
+if first_part == word:
+  print("Слово является палиндромом")
+else:
+  print("Слово не является палиндромом")
