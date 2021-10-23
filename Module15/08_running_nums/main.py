823ae2cf058fd85ea705f68139c99728da3a4cc8
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:08:22 +0500

    Выполнил 8 задание 15 модуля

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+string = list(input("Введите список: "))
+K = int(input("Введите сдвиг: "))
+
+while True:
+  for k in range(K):
+    for i in range(1, len(string)):
+      string[i - 1], string[i] = string[i], string[i - 1]
+  print(" ".join(string))
+  input("Нажмите любую кнопку!")
