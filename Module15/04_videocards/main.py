86b7f93a7a3a8a522b24712f1fc3de8fda5efa4f
dvasKolbas <karelin174@gmail.com>
2021-10-23 20:06:27 +0500

    Выполнил 4 задание 15 модуля

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,23 @@
-# TODO здесь писать код
+def input_cards(count, cards):
+    max_card = 0
+    for i in range(count):
+        card = int(input(str(i + 1) + " Видеокарта: "))
+        if card > max_card:
+            max_card = card
+        cards.append(card)
+    return cards, max_card
 
 
+def print_cards(cards, max_card):
+    print("\nСтарый список видеокарт:", cards)
+    new_cards = []
+    for i in range(len(cards)):
+        if cards[i] != max_card:
+            new_cards.append(cards[i])
+    print("\nНовый список видеокарт:", new_cards)
+
+
+count = int(input("Кол-во видеокарт: "))
+cards = []
+cells, max_card = input_cards(count, cards)
+print_cards(cards, max_card)
