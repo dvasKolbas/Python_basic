f594ec22af2414bda28e51a2b3007733eed2eea4
dvasKolbas <karelin174@gmail.com>
2021-10-25 12:52:52 +0500

    Выполнил 5 задание 16 модуля

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -1,3 +1,18 @@
+def choice(num, duration):
+    song = input(f"Название {num} песни: ")
+    index = find_song(song)
+    if index == -1:
+        print("Такой песни в списке нет!")
+    else:
+        duration += violator_songs[index][1]
+        num += 1
+    return num, duration
+
+def find_song(song):
+    for i in range(len(violator_songs)):
+        if violator_songs[i][0] == song:
+            return i
+    return -1
 violator_songs = [
     ['World in My Eyes', 4.86],
     ['Sweetest Perfection', 4.43],
@@ -9,5 +24,10 @@ violator_songs = [
     ['Blue Dress', 4.29],
     ['Clean', 5.83]
 ]
+count = int(input("Сколько песен выбрать? "))
+num = 1
+duration = 0
+while num <= count:
+    num, duration = choice(num, duration)
 
-# TODO здесь писать код
+print(f"\nОбщее время звучания песен: {round(duration, 2)} минут")
