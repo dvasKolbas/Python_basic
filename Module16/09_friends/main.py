41ffde05746b60accd0928b3b090c1e12ee4a8d7
dvasKolbas <karelin174@gmail.com>
2021-10-26 10:54:17 +0500

    Выполнил 9 задание 16 модуля

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def create_credit(credits):
+    for i in range(1, credits + 1):
+        print()
+        print(i, "расписка")
+        debtor = int(input("Кому: "))
+        creditor = int(input("От кого: "))
+        summ = int(input("Сколько: "))
+        balance[debtor - 1] -= summ
+        balance[creditor - 1] += summ
+
+def create_balace(friends):
+    for i in range(friends):
+        balance.append(0)
+
+def print_balance():
+    print("\nБаланс друзей:")
+    for i in range(len(balance)):
+        print(i + 1, ":", balance[i])
+
+balance = []
+friends = int(input("Кол-во друзей: "))
+create_balace(friends)
+credits = int(input("Долговых расписок: "))
+create_credit(credits)
+print_balance()
