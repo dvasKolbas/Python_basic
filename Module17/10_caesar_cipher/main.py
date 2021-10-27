a8372d9530bcb5ddee40ccb77874a37fec4033a8
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:31:32 +0500

    Выполнил 10 задание 17 модуля

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def coding_text(text, a, K):
+  new_text = [a[a.index(i) + K - 33] if i != " " else " " for i in text]
+  print("".join(new_text))
+
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+text = input("Введите текст: ")
+K = int(input("Введите сдвиг: "))
+coding_text(text, alphabet, K)
