72502d50dfbb20620042c80dd445e34a0671e4e7
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:22:49 +0500

    Выполнил 1 задание 17 модуля

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+vowels = "уеаоэяию"
+text = input("Введите текст: ")
+text_vowels = [l for l in text if vowels.count(l) > 0]
+print("Список гласных букв:", text_vowels)
+print("\nДлина списка:", len(text_vowels))
