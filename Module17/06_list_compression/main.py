25247aeff62fad0774c26975ee8c8b37ebffc35e
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:26:30 +0500

    Выполнил 6 задание 17 модуля

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+import random
+def change_list(listt):
+  for _ in range(listt.count(0)):
+    listt.remove(0)
+    listt.append(0)
+  return listt
+
+def delete_zero(listt):
+  for _ in range(listt.count(0)):
+    listt.remove(0)
+  return listt
+
+N = int(input("Введите кол-во элементов: "))
+
+list_N = [random.randint(0,2) for _ in range(N)]
+print("Начальный список:", list_N)
+list_N = change_list(list_N)
+print("Нули в конце:", list_N)
+list_N = delete_zero(list_N)
+print("Без нулей:", list_N)
