e08c50e7d92ca9d9c486f2242f0da91d4d59a287
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:30:30 +0500

    Выполнил 8 задание 17 модуля

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import random
+
+def throw(list_N, count):
+  start = random.randint(0, 9)
+  stop = random.randint(start, 9)
+  print(f"Бросок {count}. Сбиты палки с номера {start + 1} по номер {stop + 1}")
+  for i in range(start, stop + 1):
+    list_N[i] = "."
+  count += 1
+  return list_N, count
+
+
+N = int(input("Введите кол-во палочек: "))
+K = int(input("Введите кол-во бросков: "))
+list_N = ["|" for _ in range(N)]
+count = 1
+while count <= K:
+  list_N, count = throw(list_N, count)
+print("Результат:", "".join(list_N))
