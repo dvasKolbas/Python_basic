e64ce095b0833c74e1847a02c32f0675f087bc85
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:31:04 +0500

    Выполнил 9 задание 17 модуля

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+res_list = [nice_list[i][j][k] for i in range(len(nice_list)) for j in range(len(nice_list[i])) for k in range(len(nice_list[i][j]))]
+
+print(res_list)
