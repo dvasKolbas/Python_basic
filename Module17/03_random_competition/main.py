d579b4b44be56a07470fe50261330067f56fddc5
dvasKolbas <karelin174@gmail.com>
2021-10-27 17:24:28 +0500

    Выполнил 3 задание 17 модуля

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+import random
+team1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+team2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+team3 = [max(team1[i], team2[i]) for i in range(20)]
+print("Первая команда:",team1)
+print("\nВторая команда:",team2)
+print("\nПобедители тура:",team3)
