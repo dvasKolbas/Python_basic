ea535374b5b0fac4529f1ba6e4747889b3cb2946
dvasKolbas <karelin174@gmail.com>
2021-10-29 00:31:46 +0500

    Выполнил 9 задание 18 модуля

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def change_word(word):
+  abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+  new_word = ""
+  start = 0
+  stop = 0
+  for i in range(len(word)):
+    if word[i].lower() in abc:
+        start = i
+        if i == 0:
+            stop = (len(word) + 1) * -1
+    else:
+        new_word += word[start:stop:-1] + word[i]
+        start = i
+        stop = i
+  new_word += word[start:stop:-1]
+  return new_word
+
+msg = input("Введите сообщение: ").split()
+print(msg)
+for j in range(len(msg)):
+  msg[j] = change_word(msg[j])
+
+print(msg)
