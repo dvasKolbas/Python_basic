6596eaf8d8463d5ef01cfb3346d2a074b9d22cc5
dvasKolbas <karelin174@gmail.com>
2021-10-29 00:33:21 +0500

    Выполнил 3 задание 18 модуля

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def check_start(file):
+  start = "@№$%^&*()"
+  for i in start:
+    if file.startswith(i):
+      print(f"Ошибка, файл не может начинаться с {i}\n")
+      return True
+  return False
+
+def check_end(file):
+  end = [".txt", ".docx"]
+  for i in end:
+    if file.endswith(i):
+      return False
+  print(f"Ошибка, файл должен иметь расширение {end}\n")
+  return True
+
+cont = True
+while cont:
+  file = input("Введите название файла: ")
+  cont = check_start(file)
+  if not cont:
+    cont = check_end(file)
+
+print("Файл назван верно")
