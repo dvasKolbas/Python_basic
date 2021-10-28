08da2245e2fa5ad8636caefeecf4e31da3c4b64f
dvasKolbas <karelin174@gmail.com>
2021-10-29 00:34:06 +0500

    Выполнил 4 задание 18 модуля

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+text = input("Введите текст: ").split()
+text_list = [[i[j] if j != 0 else i[j].upper() for j in range(len(i))] for i in text]
+text_list = ["".join(i) for i in text_list]
+print(" ".join(text_list))
