b3be7e3fc9617221cf4633aae98d3f5b4aa8e50a
dvasKolbas <karelin174@gmail.com>
2021-10-29 00:35:43 +0500

    Выполнил 8 задание 18 модуля

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def shift(lst):
+  lst = [lst[i] for i in range(-1, len(lst)-1)]
+  return lst
+
+string1 = input("Первая строка: ")
+string2 = list(input("Вторая строка: "))
+for i in range(len(string2)):
+  if "".join(string2) == string1:
+    print(f"Первая строка получается из второй со сдвигом {i}")
+    break
+  string2 = shift(string2)
+else:
+  print("Первую строку нельзя получить из второй с помощью циклического сдвига.")
