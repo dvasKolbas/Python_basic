d0ed6df0907a71c907653a86e73c1f8ff53277ff
dvasKolbas <karelin174@gmail.com>
2021-10-30 20:26:28 +0500

    Изменил 4 задание 18 модуля, добавил метод title

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,4 +1,5 @@
 text = input("Введите текст: ").split()
-text_list = [[i[j] if j != 0 else i[j].upper() for j in range(len(i))] for i in text]
-text_list = ["".join(i) for i in text_list]
+#text_list = [[i[j] if j != 0 else i[j].upper() for j in range(len(i))] for i in text]
+text_list = [i.title() for i in text]
+#text_list = ["".join(i) for i in text_list]
 print(" ".join(text_list))
