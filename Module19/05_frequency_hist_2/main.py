935b5010fe382aca1b06393351f2b08ce4f5502c
dvasKolbas <karelin174@gmail.com>
2021-10-31 17:43:59 +0500

    Выполнил 5 задание 19 модуля

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def check_letter(text):
+    letter = set(text)
+    count = {}
+    for i in letter:
+        count[text.count(i)] = count.get(text.count(i), "") + i
+
+    return count
+
+text = input("Введите текст: ")
+print(check_letter(text))
