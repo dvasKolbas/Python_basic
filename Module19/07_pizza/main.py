edb5f6f0e0d70d523ee035de06d800788f70ed9a
dvasKolbas <karelin174@gmail.com>
2021-11-01 02:08:58 +0500

    Выполнил 10 задание 19 модуля

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def create_dict(count):
+    orders = {}
+    for i in range(count):
+        order_list = input(f"{i + 1} заказ: ").split()
+        orders[order_list[0]] = {order_list[1]: order_list[2]}
+    print(orders)
+
+create_dict(6)
