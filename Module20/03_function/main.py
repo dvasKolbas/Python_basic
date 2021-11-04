ecd0c74e5a1ee3949acc80ffbb6b50ad332b4b7d
dvasKolbas <karelin174@gmail.com>
2021-11-04 20:56:45 +0500

    Выполнил 3 задание 20 модуля

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -4,6 +4,7 @@ def main_f(tpl, n):
     inpt = False
     start = -1
     stop = len(tpl)
+    #можно сделать через tpl.index(), найти первое вхождение и второе вхождение n, если они есть
     if tpl.count(n) == 0:
         return tuple()
     for i_index, i_value in enumerate(tpl):
@@ -12,8 +13,7 @@ def main_f(tpl, n):
         elif i_value == n and start != -1:
             stop = i_index + 1
             break
-    new_tpl = tpl[start:stop]
-    return new_tpl
+    return tpl[start:stop]
 
 tpl = tuple(random.randint(1,5) for _ in range(10))
 n = random.randint(1,5)
