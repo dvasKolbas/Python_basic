1564d24c1b50f6851ffbe8072ff3b898e9dea72e
dvasKolbas <karelin174@gmail.com>
2021-11-09 03:13:24 +0500

    Выполнил 9 задание 20 модуля

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def create_protocol(n):
+    # temp = [[69485, "Jack"], [95715, "qwerty"], [95715, "Alex"], [83647, "M"],
+    #         [197128, "qwerty"], [95715, "Jack"], [93289, "Alex"], [95715, "Alex"], [95715, "M"]]
+    protocol = {}
+    print("Записи (результат и имя):")
+    for i in range(n):
+        data = input(f"{i + 1} запись: ").split()
+        #data = temp[i]
+        protocol[data[1]] = int(data[0])
+    print_winer(protocol)
+
+def print_winer(protocol):
+    sort_value = sorted(protocol.values(), reverse=True)[:3]
+    print("\nИтоги соревнований:")
+    for i in range(3):
+        for p_key, p_value in protocol.items():
+            if sort_value[i] == p_value:
+                print(f"{i + 1} место. {p_key} ({p_value})")
+                protocol.pop(p_key)
+                break
+
+n = int(input("Сколько записей вносится в протокол? "))
+create_protocol(n)
