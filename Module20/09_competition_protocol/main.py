5c1458dd2b760524035cb770eaa44c15bb8cdbe8
dvasKolbas <karelin174@gmail.com>
2021-11-09 17:31:31 +0500

    Изменил 9 задание 20 модуля, добавил другой вариант работы.

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -19,5 +19,25 @@ def print_winer(protocol):
                 protocol.pop(p_key)
                 break
 
-n = int(input("Сколько записей вносится в протокол? "))
-create_protocol(n)
+def second_variant(n):
+    temp = [[69485, "Jack"], [95715, "qwerty"], [95715, "Alex"], [83647, "M"],
+            [197128, "qwerty"], [95715, "Jack"], [93289, "Alex"], [95715, "Alex"], [95715, "M"]]
+    protocol = {}
+    print("Записи (результат и имя):")
+    for i in range(n):
+        #data = input(f"{i + 1} запись: ").split()
+        data = temp[i]
+        protocol[data[1]] = [int(data[0]), i + 1]
+    print(protocol)
+    scores = list(protocol.items())
+    scores.sort(key=score_key, reverse=True)
+    print(scores)
+
+
+def score_key(a):
+    return a[1][0]*100000000 - a[1][1]
+
+
+#n = int(input("Сколько записей вносится в протокол? "))
+#create_protocol(n)
+second_variant(9)
