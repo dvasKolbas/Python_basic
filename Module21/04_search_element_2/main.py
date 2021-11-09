9a8bca4a63de67ca3aa871bf1e0c09dcae76f6ba
dvasKolbas <karelin174@gmail.com>
2021-11-09 21:31:40 +0500

    Выполнил 4 задание 21 модуля

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+def find_key(depth, key, site):
+  if depth > 0:
+    if key in site:
+      return site[key]
+
+    for value in site.values():
+      if isinstance(value, dict):
+        res = find_key(depth - 1, key, value)
+        if res:
+          return res
+    else:
+      return None
+
+key = input("Введите ключ: ")
+depth = int(input("Введите грлубину: "))
+value = find_key(depth, key, site)
+if value:
+  print(value)
+else:
+  print("На такой глубине сайта, ключа нет!")
+
