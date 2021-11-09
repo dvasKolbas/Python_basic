01977319b03d6822818f809f3f02a8a994cda889
dvasKolbas <karelin174@gmail.com>
2021-11-09 23:37:06 +0500

    Выполнил 5 задание 21 модуля

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,16 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+def calculating_math_func(data, data_result, result = 1):
+    if data in data_result.keys():
+        result = data_result[data]
+    else:
+        for index in range(1, data + 1):
+            result *= index
+        data_result[data] = result
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+data_result = {}
+while True:
+    data = int(input("Введите число: "))
+    result = calculating_math_func(data, data_result)
+    print(result)
