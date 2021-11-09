b151f1cab17826cb55814c2421b04bba7157290a
dvasKolbas <karelin174@gmail.com>
2021-11-09 17:41:00 +0500

    Изменил 10 задание 20 модуля, добавил универсальность.

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,4 +1,6 @@
 def zip_func(f_block, s_block):
+    f_block = list(f_block)
+    s_block = list(s_block)
     a = ((f_block[i], s_block[i]) for i in range(min(len(f_block), len(s_block))))
     print(a)
     for i in a:
@@ -6,5 +8,9 @@ def zip_func(f_block, s_block):
     print()
 
 string = "abcd"
+tup = (10, 20, 30, 40)
+zip_func(string, tup)
+
+string = {1:2, 3:4, 5:6}
 tup = (10, 20, 30, 40)
 zip_func(string, tup)
