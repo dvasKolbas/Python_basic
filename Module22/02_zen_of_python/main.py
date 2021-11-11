b745bd135f3794fe188844688c7f3786ec5aad96
dvasKolbas <karelin174@gmail.com>
2021-11-12 02:21:10 +0500

    Выполнил 2 задание 22 модуля

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+zen_file = open("zen.txt", "r")
+reverse_zen = ""
+for line in zen_file:
+    if not ("\n" in line):
+        line = line + "\n"
+    reverse_zen = line + reverse_zen
+print(reverse_zen)
+zen_file.close()
