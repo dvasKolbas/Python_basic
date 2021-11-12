564c0b968a7940617632f49d900a475a44c10147
dvasKolbas <karelin174@gmail.com>
2021-11-12 17:55:59 +0500

    Выполнил 6 задание 22 модуля

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def cipher_text(file, a = "abcdefghijklmnopqrstuvwxyz"):
+    new_text = ""
+    for line_id, line in enumerate(file):
+        new_text += "".join([a[a.index(i.lower()) + (line_id + 1)]
+                             for i in line if i.lower() in a]).title() \
+                    + "\n"
+    return new_text
+
+file = open("text.txt", "r")
+cipher_file = open("cipher_file.txt", "w")
+cipher_file.write(cipher_text(file))
+file.close()
+cipher_file.close()
