208c97f914131c26eb887be0d6b3bf2f1efea896
dvasKolbas <karelin174@gmail.com>
2021-11-14 22:49:50 +0500

    Выполнил 1 задание 23 модуля

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+length = 0
+
+with open("people.txt", "r") as people:
+    for id, line in enumerate(people):
+        string = "".join(i for i in line if i.isalpha())
+        try:
+            if len(string) < 3:
+                raise BaseException
+            length += len(string)
+        except BaseException:
+            print(f"Длина строки {id}, меньше 3х символов")
+print("Длина символов:", length)
