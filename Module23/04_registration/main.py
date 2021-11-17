f2bf351757e54c738648da9a3daa52174195b4a9
dvasKolbas <karelin174@gmail.com>
2021-11-17 22:41:42 +0500

    Изменил 4 задание 23 модуля, добавил единоразовое открытие файлов

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -17,33 +17,34 @@ def check_age(age):
     raise ValueError
 
 def create_log(file, user):
-    with open(file, "a") as file:
         file.write(" ".join(user) + "\n")
 
 with open("registrations.txt", "r") as registration:
-    for num, line in enumerate(registration):
-        user = line.split()
-        file = "registrations_good.log"
-        try:
-            check_count(user)
-            check_name(user[0])
-            check_email(user[1])
-            check_age(user[2])
-        except IndexError:
-            print(f"В строке {num + 1}, некорректное количество полей!")
-            user.append("IndexError")
-            file = "registrations_bad.log"
-        except NameError:
-            print(f"В строке {num + 1}, некорректное имя!")
-            user.append("NameError")
-            file = "registrations_bad.log"
-        except SyntaxError:
-            print(f"В строке {num + 1}, некорректный email!")
-            user.append("SyntaxError")
-            file = "registrations_bad.log"
-        except ValueError:
-            print(f"В строке {num + 1}, некорректный возраст!")
-            user.append("ValueError")
-            file = "registrations_bad.log"
-        finally:
-            create_log(file, user)
+    with open("registrations_bad.log", "a") as file_bad:
+        with open("registrations_bad.log", "a") as file_good:
+            for num, line in enumerate(registration):
+                user = line.split()
+                file = file_good
+                try:
+                    check_count(user)
+                    check_name(user[0])
+                    check_email(user[1])
+                    check_age(user[2])
+                except IndexError:
+                    print(f"В строке {num + 1}, некорректное количество полей!")
+                    user.append("IndexError")
+                    file = file_bad
+                except NameError:
+                    print(f"В строке {num + 1}, некорректное имя!")
+                    user.append("NameError")
+                    file = file_bad
+                except SyntaxError:
+                    print(f"В строке {num + 1}, некорректный email!")
+                    user.append("SyntaxError")
+                    file = file_bad
+                except ValueError:
+                    print(f"В строке {num + 1}, некорректный возраст!")
+                    user.append("ValueError")
+                    file = file_bad
+                finally:
+                    create_log(file, user)
