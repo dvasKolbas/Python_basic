7fe5f9a159a6addd48dfdc803095285e9b25248b
dvasKolbas <karelin174@gmail.com>
2021-11-21 23:10:38 +0500

    Выполнил 3 задание 24 модуля

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+from circle import Circle_list
+
+new_Circle_list = Circle_list()
+new_Circle_list.print_circles()
+new_Circle_list.increase_circles(2)
+new_Circle_list.check_intersections()
