b79a827c3e3ae577646cbfed6ed69eb27520ee80
dvasKolbas <karelin174@gmail.com>
2021-11-23 23:37:20 +0500

    Выполнил 6 задание 24 модуля

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import random
+
+import Material
+
+material_list = [Material.Water(), Material.Wind(), Material.Fire(), Material.Earth(), Material.Human()]
+count = 10
+while count > 0:
+    a = random.choice(material_list)
+    b = random.choice(material_list)
+    print("{} + {} = {}".format(a.name, b.name, a + b))
+    count -= 1
