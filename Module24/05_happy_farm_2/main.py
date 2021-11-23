e4c73b981772f1f1819e6c53b82862461ea56fee
dvasKolbas <karelin174@gmail.com>
2021-11-23 11:24:57 +0500

    Выполнил 5 задание 24 модуля

--- Module24/05_happy_farm_2/main.py
+++ Module24/05_happy_farm_2/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+from garden import Gardener
+
+name = input("Введите имя садовника: ")
+my_gardener = Gardener(name, 5)
+while True:
+    count = int(input("Сколько картошки вы хотите получить? Не менее "))
+    my_gardener.harvest_from_garden(count)
