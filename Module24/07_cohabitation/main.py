8320dee95967a18448fb583b8230598d0204934c
dvasKolbas <karelin174@gmail.com>
2021-11-24 12:27:46 +0500

    Выполнил 7 задание 24 модуля

--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from human import Human
+
+first_human = Human("Алекс")
+second_human = Human("София")
+
+day = int(input("Введите кол-во дней: "))
+for i in range(1, day + 1):
+    if first_human.action() or second_human.action():
+        print(f"на {i} день")
+        break
+
+    if not first_human.check_all() or not second_human.check_all():
+        print(f"закончились ресурсы на {i} день")
+        break
+else:
+    print(f"Мы прожили {day} дней")
