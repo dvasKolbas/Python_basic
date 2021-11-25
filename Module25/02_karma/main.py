6b8d7bea00d100257f478fab56995699fcaaa6ec
dvasKolbas <karelin174@gmail.com>
2021-11-26 00:09:27 +0500

    Выполнил 2 задание 25 модуля

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+from karma import MyKarma
+
+if "karma.log" in os.listdir():
+    os.remove(os.path.join("karma.log"))
+
+with open("karma.log", "a") as file:
+    my_karma = MyKarma(file)
+    count_day = 0
+    while True:
+        count_day += 1
+        try:
+            if my_karma.one_day():
+                break
+        except:
+            pass
+    print("Вы достигли просвещения за ", count_day, "дней")
