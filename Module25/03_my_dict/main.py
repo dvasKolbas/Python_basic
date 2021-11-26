3dbfb37ac5506a175ea94b1f4f2e04a931116252
dvasKolbas <karelin174@gmail.com>
2021-11-26 19:49:41 +0500

    Выполнил 3 задание 25 модуля

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+class MyDict(dict):
+  def get(self, key):
+      value = super(MyDict, self).get(key)
+      if value == None:
+          return 0
+      return value
+
+dct = MyDict({1:2, 2:3, 3:4})
+print(dct.get(4))
