bdf7e2c539745cb1271225e40adc0cd8becc1c58
dvasKolbas <karelin174@gmail.com>
2021-11-27 16:47:28 +0500

    Выполнил 4 задание 25 модуля

--- Module25/04_company/main.py
+++ Module25/04_company/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import random
+
+import person
+
+peoples = [person.Manager("Вадим", "Иванов", random.randint(20, 50), 13000),
+           person.Manager("Максим", "Петров", random.randint(20, 50), 13000),
+           person.Manager("Василий", "Сидоров", random.randint(20, 50), 13000),
+           person.Agent("Иван", "Кузнецов", random.randint(20, 50), 130000),
+           person.Agent("Мария", "Иванова", random.randint(20, 50), 150000),
+           person.Agent("Руслан", "Зайцев", random.randint(20, 50), 230000),
+           person.Worker("Анна", "Васильева", random.randint(20, 50), 50),
+           person.Worker("Михаил", "Сергеев", random.randint(20, 50), 100),
+           person.Worker("Алексей", "Соболев", random.randint(20, 50), 150)]
+
+for people in peoples:
+     people.print_salary()
