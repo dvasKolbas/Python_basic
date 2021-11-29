9547727945193a5142561a82c7adc4e45e785de0
dvasKolbas <karelin174@gmail.com>
2021-11-30 02:47:39 +0500

    Выполнил 5 задание 25 модуля

--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import auto
+
+new_bus = auto.Bus(0, 0)
+while True:
+    new_bus.station()
+    angle = int(input("Введите угол перемещения автобуса: "))
+    distance = int(input("Введите расстояние перемещения автобуса: "))
+    new_bus.change_direction(angle, distance)
