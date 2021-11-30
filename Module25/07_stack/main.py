b9d17eaf74a3a6c82e83443a6b70abdcc011ab89
dvasKolbas <karelin174@gmail.com>
2021-12-01 01:51:54 +0500

    Выполнил 7 задание 25 модуля

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+from manager import TaskManager
+
+
+manager = TaskManager()
+manager.new_task("сделать уборку", 4)
+manager.new_task("помыть посуду", 4)
+manager.new_task("отдохнуть", 1)
+manager.new_task("поесть", 2)
+manager.new_task("сдать дз", 2)
+print(manager)
+
+manager.delete_task(4)
+manager.delete_task(1)
+print()
+print(manager)
