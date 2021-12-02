8591b5db919ea1fd7928dabf704a0b954c76df22
dvasKolbas <karelin174@gmail.com>
2021-12-02 17:38:16 +0500

    Выполнил 4 задание 26 модуля

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def sequence(n: list):
+    if n == [1, 2]:
+        return
+    for _ in range(10):
+        Q = n[-n[-1]] + n[-n[-2]]
+        n.append(Q)
+        yield Q
+
+print([i for i in sequence([1, 1])])
+
+
+
