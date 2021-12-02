993b4ababb4c58236951154f555d1b9da38ec372
dvasKolbas <karelin174@gmail.com>
2021-12-03 00:37:23 +0500

    Изменил 4 задание 26 модуля, добавил проверку интервала для запуска

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,10 +1,11 @@
 def sequence(n: list):
-    if n == [1, 2]:
+    if n == [1, 1]:
+        for _ in range(10):
+            Q = n[-n[-1]] + n[-n[-2]]
+            n.append(Q)
+            yield Q
+    else:
         return
-    for _ in range(10):
-        Q = n[-n[-1]] + n[-n[-2]]
-        n.append(Q)
-        yield Q
 
 print([i for i in sequence([1, 1])])
 
