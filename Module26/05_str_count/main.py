c8f680f31fd971d270e7934caf9edc151c205b00
dvasKolbas <karelin174@gmail.com>
2021-12-02 18:23:48 +0500

    Выполнил 5 задание 26 модуля

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+
+def find_file():
+    print(os.getcwd())
+    for elem in os.listdir(os.getcwd()):
+        if elem.endswith(".py"):
+            yield sum([i for i in line_count(elem)])
+
+def line_count(file_name):
+    with open(file_name, "r") as file:
+        for line in file.readlines():
+            if line != "\n" and not line.startswith("#"):
+                yield 1
+
+print(sum([i for i in find_file()]))
+
+
