98dea835c74c87bcc1aa726d57918d1b0c9e78f0
dvasKolbas <karelin174@gmail.com>
2021-12-03 23:21:16 +0500

    Изменил 5 задание 27 модуля, изменил вывод счетчика

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,13 +1,15 @@
+import functools
 from typing import Callable, Any
 
 def counter(func: Callable) -> Any:
     """
     Декоратор подсчета кол-ва вызовов функции
     """
+    @functools.wraps(func)
     def wrapped_func(*args, **kwargs):
         wrapped_func.count += 1
         func(*args, **kwargs)
-        print("Функция {name}, была вызвана: {count}раз".format(name=func.__name__, count=wrapped_func.count))
+        #print("Функция {name}, была вызвана: {count}раз".format(name=func.__name__, count=wrapped_func.count))
     wrapped_func.count = 0
     return wrapped_func
 
@@ -19,3 +21,4 @@ def simple_func(num):
 
 for i in range(5):
     simple_func(i)
+print("Функция {name}, была вызвана: {count}раз".format(name=simple_func.__name__, count=simple_func.count))
