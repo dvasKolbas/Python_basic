f4c731646801989379ab9e28f0d01c9c659c1078
dvasKolbas <karelin174@gmail.com>
2021-12-03 23:05:20 +0500

    Выполнил 5 задание 27 модуля

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+def counter(func: Callable) -> Any:
+    """
+    Декоратор вывода подсчета кол-ва вызовов функции
+    """
+    def wrapped_func(*args, **kwargs):
+        wrapped_func.count += 1
+        func(*args, **kwargs)
+        print("Функция {name}, была вызвана: {count}раз".format(name=func.__name__, count=wrapped_func.count))
+    wrapped_func.count = 0
+    return wrapped_func
+
+
+
+@counter
+def simple_func(num):
+    print(num)
+
+for i in range(5):
+    simple_func(i)
