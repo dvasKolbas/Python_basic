513e454d71ec612e32787298010c1a9b0c888a60
dvasKolbas <karelin174@gmail.com>
2021-12-06 11:28:09 +0500

    Выполнено 3 задание 28 модуля

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -3,26 +3,29 @@ import math
 
 class Cube:
     def __init__(self, Square):
-        self.parties = Square
+        self.__parties = Square
 
+    @property
     def sqr(self):
-        return self.parties.sqr * 6
+        return self.__parties.sqr * 6
 
     @property
     def parties(self):
-        return self.parties
+        return self.__parties
 
     @parties.setter
     def parties(self, Square):
-        self.parties = Square
+        self.__parties = Square
 
 class Square:
     def __init__(self, length):
         self.__parties = length
 
+    @property
     def sqr(self):
         return self.__parties ** 2
 
+    @property
     def perimeter(self):
         return self.__parties * 4
 
@@ -36,56 +39,61 @@ class Square:
 
 class Pyramid:
     def __init__(self, Treangle):
-        self.parties = Treangle
+        self.__parties = Treangle
 
+    @property
     def sqr(self):
-        base = Square(self.parties.base)
-        return self.parties.sqr * 4 + base.sqr
+        base = Square(self.__parties.base)
+        return self.__parties.sqr * 4 + base.sqr
 
     @property
     def parties(self):
-        return self.parties
+        return self.__parties
 
     @parties.setter
     def parties(self, Treangle):
-        self.parties = Treangle
+        self.__parties = Treangle
 
 class Treangle:
     def __init__(self, base, height):
-        self.base = base
-        self.height = height
+        self.__base = base
+        self.__height = height
         self.__p()
 
+    @property
     def sqr(self):
-        return (self.base * self.height / 2)
+        return (self.__base * self.__height / 2)
 
+    @property
     def perimeter(self):
-        return (self.base + self.__parties * 2)
+        return (self.__base + self.__parties * 2)
 
     def __p(self):
-        self.__parties = math.sqrt((self.base / 2) ** 2 + self.height ** 2)
+        self.__parties = math.sqrt((self.__base / 2) ** 2 + self.__height ** 2)
 
     @property
     def base(self):
-        return self.base
+        return self.__base
 
     @base.setter
     def base(self, base):
-        self.base = base
+        self.__base = base
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, height):
-        self.parties = height
+        self.__parties = height
 
 my_square = Square(5)
 print("Периметр квадрата, со стороной 5:", my_square.perimeter)
-# my_cube = Cube(my_square)
-# print("Площадь куба:", my_cube.sqr)
-# my_treangle = Treangle(3, 5)
-# print("Периметр треугольника, со основанием 3 и высотой 5:", my_treangle.perimeter)
-# my_pyramid = Pyramid(my_treangle)
-# print("Площадь пирамиды:", my_pyramid.sqr)
+my_cube = Cube(my_square)
+print("Площадь куба:", my_cube.sqr)
+my_treangle = Treangle(3, 5)
+print("Периметр треугольника, со основанием 3 и высотой 5:", round(my_treangle.perimeter, 1))
+my_pyramid = Pyramid(my_treangle)
+print("Площадь пирамиды:", my_pyramid.sqr)
+my_treangle.base = 2
+print("Площадь пирамиды, после изменения треугольника:", my_pyramid.sqr)
