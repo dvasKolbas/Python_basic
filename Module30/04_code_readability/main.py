8d736b4df90c3de3120481549a3d963fb38fc93c
dvasKolbas <karelin174@gmail.com>
2021-12-10 17:49:55 +0500

    Выполнил 4 задание 30 модуля

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def siple_value():
+    value = list()
+    for i in range(1000):
+        count = 0
+        for n in range(1, i + 1):
+            if i % n == 0:
+                count += 1
+        if count == 2:
+            value.append(i)
+    return value
+
+print(siple_value())
+
+print([i for i in range(1000) if len(list(n for n in range(1, i + 1) if i % n == 0)) == 2])
