f533bed8b85bb98e886824be892bd29cb2ac016e
dvasKolbas <karelin174@gmail.com>
2021-12-13 21:12:23 +0500

    Выполнил 6 задание 31 модуля

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import requests
+import re
+
+res = requests.get("http://www.columbia.edu/~fdc/sample.html")
+sample = r"(?<=>).+(?=</h3>)"
+tags = re.findall(sample, res.text)
+print(tags)
+
