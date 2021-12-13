dbf9d6795c3d61e8f672c3939731f43da72d559e
dvasKolbas <karelin174@gmail.com>
2021-12-13 12:55:34 +0500

    Выполнил 4 задание 31 модуля

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+import re
+from typing import List
+
+
+def check_numbers(nums: List) -> None:
+    sample = r"\b[8,9]\d{9}\b"
+    res = []
+    for id, num in enumerate(nums):
+        print(id + 1, end=" номер: ")
+        if re.match(sample, num):
+            print("всё в порядке")
+        else:
+            print("не подходит")
+    return
+
+
+
+
+numbers = ['9999999999', '999999-999', '99999x9999']
+check_numbers(numbers)
